3fb649f1a013081ad0429530e1e129cc31334b51
thechicken82 <ethan.nadeau@outlook.com>
2024-11-24 21:46:21 -0600

    first

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/thechicken82/car_program
+
+go 1.22.5
+
+require github.com/thechicken82/car v0.0.0-20241125033207-cbac91600ed2 // indirect
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/thechicken82/car"
+)
+
+func main() {
+
+	s1 := car.Honk()
+	s2 := car.Honks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	// also like this
+	fmt.Println(car.Honk())
+}
